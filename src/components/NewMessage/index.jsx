c285f9ca3e2edb898a1c156299c65fbe62da344b
Jonel Gonzalez <john.gonzalez0719@gmail.com>
2022-12-05 17:10:06 +0800

    Emoji added to messaging

--- src/components/NewMessage/index.jsx
+++ src/components/NewMessage/index.jsx
@@ -7,17 +7,25 @@ import userProfileStore from '../../store/userProfile';
 import { _ } from 'lodash'
 import Conversation from '../Conversation';
 import { QueryClient, QueryClientProvider } from 'react-query';
-
+import Picker from 'emoji-picker-react';
+import { MdInsertEmoticon } from 'react-icons/md';
 
 export const NewMessage = () => {
   const [message, setMessage] = useState('');
   const [displayMessage, setDisplayMessage] = useState([]);
+  const [inputStr, setInputStr] = useState('');
+  const [showPicker, setShowPicker] = useState(false);
   const { receiver, overwriteReceiver, clearReceiver } = useReceiverStore(
     state => ({
       receiver: state.receiver,
       overwriteReceiver: state.overwriteReceiver,
       clearReceiver: state.clearReceiver,
   })) 
+
+  const onEmojiClick = (e) =>{
+    setMessage(prevInput => prevInput + e.emoji);
+  }
+
   const { profile, overwriteProfile, clearProfile } = userProfileStore(
     (state) => ({
       profile: state.profile,
@@ -25,6 +33,7 @@ export const NewMessage = () => {
       clearProfile: state.clearProfile,
     })
   );
+
 
   const newMessage = e => {
     e.preventDefault();
@@ -40,6 +49,11 @@ export const NewMessage = () => {
     setMessage('');
   };
 
+
+  const toggleEmoji = () => {
+    showPicker(false);
+  }
+
   const queryConversation = new QueryClient();
   return (
     <div className="new-message flex-column">
@@ -53,8 +67,13 @@ export const NewMessage = () => {
           <Conversation receiver_id={receiver && receiver.receiver_id}/>
         </QueryClientProvider>
       </div>
+      
+      {showPicker && 
+      <div style={{position:'absolute', right:'0', top:'30%'}}> 
+         <Picker pickerStyle={{ width: '100%', position:'absolute' }} onEmojiClick={onEmojiClick} /> </div>
+        }
       <form className="message-form flex-row" onSubmit={sendMessage}>
-        <div className="message-field input-container">
+        <div className="message-field input-container" style={{position:'relative'}}>
           <input
             className="input-container__textbox"
             value={message}
@@ -62,7 +81,13 @@ export const NewMessage = () => {
             placeholder=" "
             autoComplete="off"
             autoCorrect="off"
-          ></input>
+          >
+          </input>
+          
+          <div style={{postion: 'absolute', right: '-10'}}>
+            <MdInsertEmoticon onClick={() => setShowPicker(val => !val)} />
+          </div>
+          
           <InputPlacholder display="New Message" />
         </div>
         <div className="send-message-btn">
@@ -72,6 +97,7 @@ export const NewMessage = () => {
         </div>
 
       </form>
+      
     </div>
   );
 };
